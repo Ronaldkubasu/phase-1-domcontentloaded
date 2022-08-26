8d790a435887ada1f9bd5ba5a3ce81a4e79b696c
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-08-26 05:14:44 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// document.addEventListener("DOMContentLoaded",function(event){
+//     console.log("The Dom has loaded");
+// });
+
+// console.log(
+//     "This console.log() fires when index.js loads - befofore DOMcontentLoaded is triggered"
+// );
+
+
+ document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+     let p = document.getElementById('text').textContent = "This is really cool!";
+  });
